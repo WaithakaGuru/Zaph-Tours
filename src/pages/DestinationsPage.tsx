b380cd4650ae31dba0ae3fef88689e90d53cb25e
WaithakaGuru <waithakaoffices@gmail.com>
2025-07-01 13:29:15 +0300

    Format code for readability

--- src/pages/DestinationsPage.tsx
+++ src/pages/DestinationsPage.tsx
@@ -5,7 +5,6 @@ import {
   CardMedia,
   Typography,
   Button,
-  Grid,
   Chip,
   CardActions,
   Tooltip,
@@ -19,90 +18,129 @@ import { Group, OneK } from "@mui/icons-material";
 function DestinationsPage() {
   return (
     <Container>
-      <Stack direction={'column'}>
-        <Typography variant="h3"align="center" my={3} fontSize={40} gutterBottom
-        color="primary" textTransform="uppercase" fontWeight={600}>
+      <Stack direction={"column"}>
+        <Typography
+          variant="h3"
+          align="center"
+          my={3}
+          fontSize={40}
+          gutterBottom
+          color="primary"
+          textTransform="uppercase"
+          fontWeight={600}
+        >
           Explore the most exciting destinations
         </Typography>
-        <Typography variant="h6" maxWidth={'60%'} mx={'auto'} my={3}  align="center"  gutterBottom>
-          A wide listing of all the top rated destinations by our previous travellers 
-              giving a full detailed explanation of the area with the cost and the experiences thereof. 
+        <Typography
+          variant="h6"
+          maxWidth={"60%"}
+          mx={"auto"}
+          my={3}
+          align="center"
+          gutterBottom
+        >
+          A wide listing of all the top rated destinations by our previous
+          travellers giving a full detailed explanation of the area with the
+          cost and the experiences thereof.
         </Typography>
 
-        <Box display={"grid"} gridTemplateColumns={{
-              xs: "1fr",
-              sm: "1fr 1fr",
-              md: "1fr 1fr 1fr",
-            }}  gap={3}
-          justifyContent={'center'} 
+        {/* A grid layout for the destinations cards */}
+        <Box
+          display={"grid"}
+          gridTemplateColumns={{
+            xs: "1fr",
+            sm: "1fr 1fr",
+            md: "1fr 1fr 1fr",
+          }}
+          gap={3}
+          justifyContent={"center"}
           p={3}
-          bgcolor={'#f9f9f9'} >
-            {destinations.map(destination => (
-              <Card
-                sx={{
-                  transition: "scale .4s",
-                  borderRadius: ".3rem",
-                  "&:hover": { scale: 1.05 },
-                  minHeight :'max-content',
-                  minWidth: '20rem',
-                  position:"relative"
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  image={destination.image}
-                  alt={destination.name}
-                  height={'200px'}
-                />
-                <CardContent>
-                  <Typography variant="h6" fontWeight={600} align="center" gutterBottom>
-                    {destination.name}
-                  </Typography>
-                  <Typography
-                    variant="subtitle1"
-                    align="center"
-                    fontSize={"1rem"}
-                    color="text.secondary"
+          bgcolor={"#f9f9f9"}
+        >
+          {destinations.map((destination) => (
+            <Card
+              sx={{
+                transition: "scale .4s",
+                borderRadius: ".3rem",
+                "&:hover": { scale: 1.05 },
+                minHeight: "max-content",
+                minWidth: "20rem",
+                position: "relative",
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={destination.image}
+                alt={destination.name}
+                height={"200px"}
+              />
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  fontWeight={600}
+                  align="center"
+                  gutterBottom
+                >
+                  {destination.name}
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  align="center"
+                  fontSize={"1rem"}
+                  color="text.secondary"
+                >
+                  {destination.description}
+                </Typography>
+                <Stack
+                  direction={{ xs: "column", md: "row" }}
+                  gap={2}
+                  justifyContent={"center"}
+                  alignItems={"center"}
+                  sx={{ mt: 2 }}
+                >
+                  <Chip
+                    label={`Group: ${destination.groupPrice}`}
+                    avatar={<Group />}
+                    color="primary"
+                    variant="filled"
+                    size="medium"
+                  />
+
+                  <Chip
+                    label={`Solo: ${destination.individualPrice}`}
+                    color="secondary"
+                    variant="filled"
+                    size="medium"
+                    avatar={<OneK />}
+                  />
+                  <CardActions
+                    sx={{
+                      position: "absolute",
+                      bottom: "50%",
+                      right: "1rem",
+                      py: 0,
+                    }}
                   >
-                    {destination.description}
-                  </Typography>
-                  <Stack
-                    direction={{ xs: "column", md: "row" }}
-                    gap={2}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    sx={{ mt: 2 }}
-                  >
-                    <Chip label= {`Group: ${destination.groupPrice}`}
-                      avatar={<Group/>}
-                      color="primary"
-                      variant="filled"
-                      size="medium"
-                    />
-                    
-                   <Chip label= {`Solo: ${destination.individualPrice}`}
+                    <Tooltip
+                      sx={{ py: 0 }}
+                      arrow
+                      title={`Learn more about the ${destination.name}`}
                       color="secondary"
-                      variant="filled"
-                      size="medium"
-                      avatar={<OneK/>}
-                    />
-                    <CardActions 
-                     sx={{position: 'absolute',
-                       bottom: '50%', right: '1rem', py: 0}}
                     >
-                      <Tooltip sx={ {py: 0}} arrow  
-                      title={`Learn more about the ${destination.name}`} color="secondary">
-                         <Button size="small" variant="contained"
-                          sx={{my: 0}} href="#"
-                        >
-                          Learn More
-                          </Button> 
-                      </Tooltip>
-                    </CardActions>
-                  </Stack>
-                </CardContent>
-              </Card>
-            ))}
+                      <Button
+                        size="small"
+                        variant="contained"
+                        sx={{ my: 0 }}
+                        href="#"
+                      >
+                        Learn More
+                      </Button>
+                    </Tooltip>
+                  </CardActions>
+                </Stack>
+              </CardContent>
+            </Card>
+          ))}
         </Box>
       </Stack>
     </Container>
